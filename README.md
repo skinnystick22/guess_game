84abad72341695be393fbc7be4b3d2dadbdebe95
Tyler Miller <tyler.e.miller1995@gmail.com>
2024-07-07 21:12:04 -0500

    Initial Commit
    Initial Commit
    
    Added basic structure for a guessing game in Rust. This includes the creation of .gitignore, Cargo.toml, README.md and main.rs files. The .gitignore file has been set up to ignore unnecessary files such as compiled files, backup files generated by rustfmt and MSVC Windows builds of rustc. The Cargo.toml file has been configured with package details and dependencies required for the project. The README.md file has been updated with a brief description of the application. Lastly, the main.rs file contains the core logic for the guessing game which prompts users to guess a number between 1 and 100 until they guess correctly.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# guess_game
+# Guessing Game
+This is my first application written in Rust. I am going through the book on the Rust Foundations website.~~~~
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number between 1 and 100!");
+    
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100); 
+    println!("The secret number is {secret_number}.");
+
+    loop {
+        println!("Enter your guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse()  {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+            Ordering::Greater => println!("Too big!")
+        };
+    }
+}
